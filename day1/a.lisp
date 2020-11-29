f1cd23f5152a78d57a5aef5b4a80a058fee9937f
Justin <justin2004@hotmail.com>
2020-11-29 09:56:55 -0500

    day1 p2

--- day1/a.lisp
+++ day1/a.lisp
@@ -1,6 +1,8 @@
 (ql:quickload :april)
 (ql:quickload :alexandria)
 
+(in-package :april)
+
 (setf *i* (with-open-file (s "input.txt" 
                    :direction :input)
   (loop 
@@ -9,9 +11,25 @@
     :collect line)))
 
 
-; part 1
+; get the input into april
 (april:april (with (:state :in 
                     ((i (coerce (mapcar #'parse-integer *i*) 
                                 'vector)))) )
-             "+/(⌊i÷3)-2")
+             "input←i")
+
+; part 1
+(april:april "+/(⌊input÷3)-2")
+
+
+
+;part 2
+; i can't use recursive functions until this is fixed
+; https://github.com/phantomics/april/issues/84
+(april "f←{(⌊⍵÷3)-2}")
+(april "bigf←{mass←⍵
+              successive_results←(⍳⌈3⍟mass) {(f⍣⍺)⍵}¨ mass
+              +/(successive_results>0)/successive_results}")
+
+(april:april "+/bigf¨ input")
+
 
---
+++ day1/scrap.lisp
@@ -0,0 +1,98 @@
+; part 2 prep
+(april "({⍵÷3}⍣1) 100")
+(april "f←{⍵÷3} ")
+(april "(f⍣5) 100") (april "(⍳⌈3⍟100) ∘.f 100") (april "⍳⌈3⍟100")
+(april "(⍳⌈3⍟100) {(f⍣⍺)⍵} 100")
+(april "(⍳⌈3⍟100) {(f⍣⍺)⍵}¨ 100")
+(april "1 2 {⍺+⍵} 100")
+(april "(⍳4) {(f⍣⍺)⍵}¨ 100")
+(april "1 {(f⍣⍺)⍵}¨ 100") ; nil
+(april "1 2 f¨ 100")
+(april "1 {⍵÷3}¨ 10")
+(april "1 2 3 {⍵÷3} 100")
+(april "f←{⍵÷3}")
+(april "f f 100")
+; 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346 ;; 100756
+(april "f f f 100756")
+
+; works on one
+(april "mass←100756
+        successive_results←(⍳⌈3⍟mass) {(f⍣⍺)⍵}¨ mass
+        +/(successive_results>0)/successive_results")
+
+
+
+(april "f←{⎕←a←(⌊⍵÷3)-2⋄⎕←' ' ⋄
+           $[a≥0;a,f a;0]}")
+(april "f 90")
+
+(april "f←{⎕←a←⍵-1⋄⎕←' '⋄ $[a≥0;a,f a;0]}")
+(april "f 5")
+
+
+(april "f←{((⍵∊0 1)+1)⊃'no' 'yes'}
+              f¨⍳5")
+
+(april "f←{((⍵∊0 1)+1)⊃'no' ⍎ '6-1' }
+              f 1")
+
+(april "⍎ '4'")
+(april "v←1 2 3⋄ ⍎ 'v'")
+(april (demo))
+
+(april "f←{((⍵∊0 1)+1)⊃'no' (⍎ '∇ ⍵-1') }
+              f 1")
+(april "f←{$[⍵=0;1;⍵ × (f ⍵-1)]}
+        f 3")
+
+
+
+(april "fib←{⊃(+/,⊃)⍣⍵⊢1}")
+(april "fib←{(+/,⊃)⍣1 ⍵}")
+(april "fib 8")
+
+; equiv
+(april-f "(↓⍣1)2 2 ⍴ ⍳4")
+(april "≢↓↓2 2 ⍴ ⍳4")
+(april-f "↓↓2 2 ⍴ ⍳4")
+
+(april-f "(↓⍣2)2 2 ⍴ ⍳4")
+
+(april "1 +∘÷⍣= 1")
+
+(april "f ← (32∘+)∘(×∘1.8)")
+(april "c ← f⍣¯1")
+(april "c 212")
+
+(april "(32∘+)∘(×∘1.8)⍣¯1⊢212")
+(april "(32∘+)∘(×∘1.8) 100")
+(april-f "1 2 ∘.+ 1 2")
+(april-f "1 2 +∘, 3 4")
+(april-f "1 2 ∘., 3 4")
+
+
+(april "f←{⎕←a←⍵-1⋄⎕←' '⋄ $[a≥0;a+f a;0]}")
+(april "f←{⎕←a←⍵-1⋄⎕←' '⋄ $[a>0;a+f a;0]}")
+(april "f 5")
+
+(april (demo))
+
+(april "(10 20 30@1 2 10)⍳10")
+
+
+(april "(-⍤÷) 4")
+(april "(-⍤0) 4")
+
+12 -⍤÷ 4   ⍝ (x f⍤g y) ≡ (f x g y)
+
+(april "⎕←cube←2 2 3 ⍴ ⍳100")
+(april "⎕←cube←2 3 ⍴ ⍳100") ; vols rows cols ;  rows cols
+(april-f "cube")
+(april-f "(,⍤3) cube")
+(april-f "⍴ cube")
+
+(april "a←2 2 ⍴⍳4")
+(april "b←2 2 ⍴4+⍳4")
+
+(april-f "a,[1]b")
+(april-f "+/[1]a")
