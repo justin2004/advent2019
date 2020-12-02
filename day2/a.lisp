06bc439671a21c3a93fe67a0c43cf3fc95846b9f
Justin <justin2004@hotmail.com>
2020-12-01 23:26:40 -0500

    note

--- day2/a.lisp
+++ day2/a.lisp
@@ -16,6 +16,7 @@
 
 ; part 1
 (april "input[2]←12 ⋄ input[3]←2")
+; all this "+1" business is because i didn't do ⎕IO←0 and the problem input uses 0 indexing
 (april "f←{$[99=⍺[⍵];0; ⍺[1+⍺[⍵+3]]←⍺[⍵]⊃⍺[⍺[⍵+1]+1](+,×)⍺[⍺[⍵+2]+1] ⋄ ⍺ f 4+⍵]}")
 ; i wanted to avoid if statements but i don't know how else to do a recursive function
 ; with a base case :/
